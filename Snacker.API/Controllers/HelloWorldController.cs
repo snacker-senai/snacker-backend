e7217b4f47499d1db07cc5a3c1ff46948afc4daf
Gustavo Souza <gustavo.souza@pagueveloz.com.br>
2022-05-02 19:30:59 -0300

    Controller com Hello World

---
+++ Snacker.API/Controllers/HelloWorldController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Snacker.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HelloWorldController : ControllerBase
+    {
+        private readonly ILogger<HelloWorldController> _logger;
+
+        public HelloWorldController(ILogger<HelloWorldController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            return "Hello World!";
+        }
+    }
+}

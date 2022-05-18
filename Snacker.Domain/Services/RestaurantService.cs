dd1cd4bac6016925832312c2efad04711f1732d4
Gustavo Souza <gustavojo47@gmail.com>
2022-05-18 19:17:15 -0300

    Utilizando o RestaurantDTO nos gets do RestaurantService

--- Snacker.Domain/Services/RestaurantService.cs
+++ Snacker.Domain/Services/RestaurantService.cs
@@ -1,8 +1,9 @@
-﻿using FluentValidation;
+﻿using Snacker.Domain.DTOs;
 using Snacker.Domain.Entities;
 using Snacker.Domain.Interfaces;
 using Snacker.Domain.Validators;
 using System;
+using System.Collections.Generic;
 
 namespace Snacker.Domain.Services
 {
@@ -19,6 +20,64 @@ namespace Snacker.Domain.Services
             Validate(obj, Activator.CreateInstance<RestaurantValidator>());
             _restaurantRepository.Insert(obj);
             return obj;
+        }
+
+        public override ICollection<object> Get()
+        {
+            var itens = _restaurantRepository.Select();
+            var result = new List<object>();
+            foreach (var item in itens)
+            {
+                var dto = new RestaurantDTO()
+                {
+
+                    Name = item.Name,
+                    Description = item.Description,
+                    Address = new AddressDTO()
+                    {
+                        Street = item.Address.Street,
+                        City = item.Address.City,
+                        Country = item.Address.Country,
+                        Number = item.Address.Number,
+                        CEP = item.Address.CEP,
+                        District = item.Address.District
+                    },
+                    AddressId = item.AddressId,
+                    RestaurantCategory = new RestaurantCategoryDTO()
+                    {
+                        Name = item.RestaurantCategory.Name,
+                    },
+                    RestaurantCategoryId = item.RestaurantCategoryId,
+                };
+                result.Add(dto);
+            }
+            return result;
+        }
+
+        public override object GetById(long id)
+        {
+            var item = _restaurantRepository.Select(id);
+            return new RestaurantDTO()
+            {
+
+                Name = item.Name,
+                Description = item.Description,
+                Address = new AddressDTO()
+                {
+                    Street = item.Address.Street,
+                    City = item.Address.City,
+                    Country = item.Address.Country,
+                    Number = item.Address.Number,
+                    CEP = item.Address.CEP,
+                    District = item.Address.District
+                },
+                AddressId = item.AddressId,
+                RestaurantCategory = new RestaurantCategoryDTO()
+                {
+                    Name = item.RestaurantCategory.Name,
+                },
+                RestaurantCategoryId = item.RestaurantCategoryId,
+            };
         }
     }
 }

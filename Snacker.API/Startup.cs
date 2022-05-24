54edc2d1787fafdd246c7b8b63a3b21aaa25f71c
Gus <gustavojo47@gmail.com>
2022-05-24 17:35:10 -0300

    Ajuste da rota do swagger

--- Snacker.API/Startup.cs
+++ Snacker.API/Startup.cs
@@ -108,7 +108,11 @@ namespace Snacker.API
             {
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Snacker.API v1"));
+                app.UseSwaggerUI(c =>
+                {
+                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Snacker.API v1");
+                    c.RoutePrefix = string.Empty;
+                });
             }
 
             app.UseHttpsRedirection();

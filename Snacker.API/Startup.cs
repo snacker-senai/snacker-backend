b74011cd60cc2c87b3c0bf1d61a21f5189686677
Gus <gustavojo47@gmail.com>
2022-05-25 19:37:34 -0300

    Removendo ponto do endpoint do swagger

--- Snacker.API/Startup.cs
+++ Snacker.API/Startup.cs
@@ -112,7 +112,7 @@ namespace Snacker.API
             app.UseSwagger();
             app.UseSwaggerUI(c =>
             {
-                c.SwaggerEndpoint("./swagger/v1/swagger.json", "Snacker.API v1");
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Snacker.API v1");
                 c.RoutePrefix = string.Empty;
             });
 

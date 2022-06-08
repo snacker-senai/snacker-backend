8a44e9bda7f4b96cdcbe58422a10bc3035601793
Gus <gustavojo47@gmail.com>
2022-06-08 18:55:32 -0300

    Retornando price no get de order by bill

--- Snacker.Domain/DTOs/ProductAndQuantityDTO.cs
+++ Snacker.Domain/DTOs/ProductAndQuantityDTO.cs
@@ -10,10 +10,12 @@ namespace Snacker.Domain.DTOs
             ProductId = orderHasProduct.ProductId;
             Quantity = orderHasProduct.Quantity;
             ProductName = orderHasProduct.Product.Name;
+            Price = orderHasProduct.Product.Price;
         }
 
         public long ProductId { get; set; }
         public string ProductName { get; set; }
         public int Quantity { get; set; }
+        public decimal Price { get; set; }
     }
 }

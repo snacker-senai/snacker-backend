55c6b1cca5aba7446835751d2d71eca6ef752c0f
Gus <gustavojo47@gmail.com>
2022-06-21 19:53:49 -0300

    Retornando a quantidade total de páginas na paginação de mesa

--- Snacker.API/Controllers/TableController.cs
+++ Snacker.API/Controllers/TableController.cs
@@ -78,9 +78,14 @@ namespace Snacker.API.Controllers
             {
                 var restaurantId = long.Parse(_authService.GetTokenValue(authorization.Split(" ")[1], "RestaurantId"));
 
-                return Ok(_tableService.GetFromRestaurant(restaurantId).Skip((page.HasValue ? page.Value - 1 : 0) * pageSize)
-                                             .Take(pageSize)
-                                             .ToList());
+                var tables = _tableService.GetFromRestaurant(restaurantId);
+                decimal pageQuantity = tables.Count / pageSize;
+                var totalPages = Math.Ceiling(pageQuantity);
+                var data = tables.Skip((page.HasValue ? page.Value - 1 : 0) * pageSize)
+                                              .Take(pageSize)
+                                              .ToList();
+
+                return Ok(new { data, totalPages });
             }
             catch (Exception ex)
             {

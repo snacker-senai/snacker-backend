31bcb08ce7a6942c42a28e6dda95a7fa0d4b39f6
Gus <51535780+gustavo-js@users.noreply.github.com>
2022-10-31 19:36:38 -0300

    Update ProductCategoryController.cs

--- Snacker.API/Controllers/ProductCategoryController.cs
+++ Snacker.API/Controllers/ProductCategoryController.cs
@@ -50,7 +50,7 @@ namespace Snacker.API.Controllers
             return Execute(() => _productCategoryService.Get());
         }
 
-        [Authorize(Roles = "Cliente")]
+        [Authorize(Roles = "Cliente, Admin, Garçom, Cozinheiro")]
         [HttpGet("WithProducts")]
         public IActionResult GetWithProducts([FromHeader] string authorization)
         {

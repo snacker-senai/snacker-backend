49b1314fb1125268a79820fcda549164a2ee9dd5
Gus <gustavojo47@gmail.com>
2022-12-14 00:33:52 -0300

    Ajuste bill

---
+++ Snacker.Domain/DTOs/BillDTO.cs
@@ -0,0 +1,18 @@
+﻿using Snacker.Domain.Entities;
+
+namespace Snacker.Domain.DTOs
+{
+    public class BillDTO : BaseDTO
+    {
+        public BillDTO()
+        {
+
+        }
+        public BillDTO(Bill bill)
+        {
+            Id = bill.Id;
+            Active = bill.Active;
+        }
+        public boolean Active { get; set; }
+    }
+}
